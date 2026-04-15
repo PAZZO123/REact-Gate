e27f6af74aa2cd12dd915397e3a72e35b6e421e4
PAZZO123 <smbabazipatrick@gmail.com>
2026-04-15 22:44:35 +0200

    Initial Commit and last one

---
+++ react-gate-app/src/App.jsx
@@ -0,0 +1,13 @@
+import TwoStepForm from "./Components/TwoStep"
+export default function App(){
+  return(
+    <>
+ 
+    <div className=" flex flex-col justify-center items-center shadow-sm shadow-mauve-600  p-14 m-6">
+  <TwoStepForm/>
+   
+ 
+    </div>
+    </>
+  )
+}
